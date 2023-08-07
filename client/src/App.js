78639fe9c12f9ab43d69d0b978b9e336321c8295
Liam Gelfand <gelfand.liam@gmail.com>
2023-08-06 21:45:24 -0400

    Revert "Merge branch 'main' of https://github.com/liamgelfand/grade-calculator"
    This reverts commit 4e1b360c0fa01aff617a497ba77132970f3c3d3a, reversing
    changes made to ee82af5441b16ff8ab2fc948e3f13d545c933b8f.

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React, {useEffect} from "react";
 import "./App.css";
 
 export default function App() {
-  const [numSections, setNumSections]=React.useState(0)
-  const [classes, setClasses]=React.useState([{name:'', sections:[{section:'', weight:'', grades:''}]}])
+  const [numAssignments, setNumAssignments]=React.useState(0)
+  const [classes, setClasses]=React.useState([{name:'', assignments:[{section:'', weight:'', grades:''}]}])
   const [studentName, setStudentName]=React.useState("")
 
 const addClass = () => {
@@ -12,7 +12,7 @@ const addClass = () => {
 }
 
 
-const handleCourseChange = (courseIndex, sectionIndex, event) => {
+const handleCourseChange = (courseIndex,assignmentIndex, event) => {
   let classData = [...classes];
   if (event.target.name==='name'){
     classData[courseIndex][event.target.name] = event.target.value;
@@ -38,11 +38,10 @@ function submit(e) {
   // creates shallow copy of classes data
   let classData = [...classes];
   e.preventDefault();
-  let objectToSend={student:studentName,
-  courses:classData}
-  console.log("you are about to send over", objectToSend);
+  console.log("you are about to send over", classData);
 
-  const requestBody = JSON.stringify(objectToSend);
+  // prepares data to send to api
+  const requestBody = JSON.stringify(classData);
   const apiEndpoint = 'http://localhost:3001/gradecalc/addclass';
 
   // calls api
@@ -63,28 +62,16 @@ function submit(e) {
     });
 }
 
-const changeText = (e) => {
-  //setTextValue(e.target.value);
-  setStudentName(e.target.value);
-  }
 
 
 return (
   <div className="large-box">
     <h1 className="title">Grade Calculator</h1>
-    <form onSubmit={(e) => submit(e)}>
-    <input
-              className='class-name'
-              name='student-name'
-              placeholder='Student Name'
-              value={studentName}
-              onChange={changeText}
-            />
     <div className="add-class-container">
       <p className="add-class-text">Add Class</p>
       <button className="add-class-button" onClick={addClass} label="Add Class">+</button>
     </div>
-  
+    <form onSubmit={(e) => submit(e)}>
       {classes.map((input, courseIndex) => {
         return (
           <div key={courseIndex} className='course'>
